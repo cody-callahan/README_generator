f273aab7e12dbbbdbff33ef4eaf41d320483e329
Cody Callahan <codycallahan02@gmail.com>
2021-08-04 17:25:05 -0700

    add to generate markdown function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./Develop/utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
@@ -106,11 +107,17 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+    fs.writeFile('./README.txt', data, err => {
+        if (err) throw new Error(err);
+        console.log('Page created! Check out index.html in this directory to see it!');
+      })
+};
 
 // TODO: Create a function to initialize app
 const init = () => {
     inquirer.prompt(questions).then(answers => {
+        writeToFile(generateMarkdown(answers));
         console.log(answers);
     });
 };
